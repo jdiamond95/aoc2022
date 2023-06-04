d5dbf375912ec4da21d9c0018fd9ed95b539fc59
Jac Diamond <jac@canva.com>
2023-06-04 19:55:23 +1000

    Day 3 part 1

--- src/day03.rs
+++ src/day03.rs
@@ -2,19 +2,22 @@ use std::collections::HashSet;
 
 pub fn run(input: &str) {
     println!("\n\nDay 03");
-    let split_compartments: &[&[u8]] = input
+    let split_compartments = input
         .lines()
-        .map(|rucksack| rucksack.as_bytes().split_at(rucksack.len() / 2))
-        .collect();
+        .map(|rucksack| rucksack.as_bytes().split_at(rucksack.len() / 2));
 
-    let _total_priority = 0;
+    let mut total_priority: u32 = 0;
     for bag in split_compartments {
-        println!("{:?}", bag);
+        let cmp1: HashSet<_> = bag.0.iter().collect();
+        let cmp2: HashSet<_> = bag.1.iter().collect();
+        let intersection: Vec<_> = cmp1.intersection(&cmp2).collect();
+        total_priority += u32::from(translate_priority(**intersection[0]));
     }
+    println!("{total_priority}");
 }
 
-fn _translate_priority(letter: char) -> u32 {
-    let mut priority = letter as u32;
+fn translate_priority(letter: u8) -> u8 {
+    let mut priority = letter.clone();
     match priority {
         65..=90 => priority -= 38,
         97..=122 => priority -= 96,
